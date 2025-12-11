b736a624ddf278c86454c29c69bf37cf0c101aed
Vincent COUVERT <vincent.couvert@3ds.com>
2025-12-11 09:30:07 +0100

    Fix copyright after !1591, this file did not exist before.
    See https://gitlab.com/scilab/scilab/-/merge_requests/1614

--- scilab/modules/graphics/src/cpp/setGetHashTable.cpp
+++ scilab/modules/graphics/src/cpp/setGetHashTable.cpp
@@ -1,17 +1,10 @@
 ﻿/*
  * Scilab ( https://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2006 - INRIA - Jean-Baptiste Silvy
- * Copyright (C) 2007 - INRIA - Vincent Couvert
+ * Copyright (C) 2025 - Dassault Systèmes S.E. - Antoine ELIAS
+ * Copyright (C) 2025 - Dassault Systèmes S.E. - Clément DAVID
  *
- * Copyright (C) 2012 - 2016 - Scilab Enterprises
- *
- * This file is hereby licensed under the terms of the GNU GPL v2.0,
- * pursuant to article 5.3.4 of the CeCILL v.2.1.
- * This file was originally licensed under the terms of the CeCILL v2.1,
- * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
- *
  */
 
 /*------------------------------------------------------------------------*/
